cb6f55f5b9d1e694d33d6d1af9f7126428472b4b
Sanjeev Tripathi <sanjeevmail309@gmail.com>
2020-06-20 20:50:47 +0530

    feat(added vid2frames): script to convert video to frames for annotation

---
+++ vid2frames.py
@@ -0,0 +1,52 @@
+import os
+import cv2
+import time
+import config
+
+
+class Vid2Frames:
+
+    def __init__(self, in_path, out_path):
+
+        for _, __, files in os.walk(in_path):
+            for file in files:
+                print("[INFO] Processing: ", file)
+                path = os.path.join(in_path, file)
+                self.load_video(path)
+                self.save_frames(self.video, out_path)
+
+    # def __del__(self):
+    #     del self.video
+    #     cv2.destroyAllWindows()
+
+    def load_video(self, path):
+        self.video = None
+
+        if os.path.exists(path):
+            self.video = cv2.VideoCapture(path)
+
+    def save_frames(self, video, out_path, visual=False):
+
+        base_filename = "image_"
+        file_index = 0
+
+        while video.isOpened():
+
+            ret, frame = video.read()
+            if frame is not None:
+                if visual:
+                    cv2.imshow('frame', frame)
+                    if cv2.waitKey(1) & 0xFF == ord('q'):
+                        break
+
+                filename = base_filename + str(file_index) + ".jpg"
+                cv2.imwrite(os.path.join(out_path, filename), frame)
+
+                file_index += 1
+
+        print("[INFO] Saved images", file_index)
+        self.video.release()
+
+
+if __name__ == "__main__":
+    Vid2Frames(config.INPUT_VIDEO_PATH, config.OUTPUT_FRAMES_PATH)
