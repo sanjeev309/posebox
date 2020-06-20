2f7375456bc65c86bcb96c36f103f14edde7d142
HarshiniGudipally <mailharshinig@gmail.com>
2020-06-20 22:56:50 +0530

    feat(vid2frames.py):capture one frame for every five frames in a video

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ This python script holds configuration for all variables to be used in this proj
 """
 
 # Vid 2 Frames
-INPUT_VIDEO_PATH = "video"
+INPUT_VIDEO_PATH = "videos"
 OUTPUT_FRAMES_PATH = "frames"
+VISUAL = False
--- vid2frames.py
+++ vid2frames.py
@@ -6,7 +6,7 @@ import config
 
 class Vid2Frames:
 
-    def __init__(self, in_path, out_path):
+    def __init__(self, in_path, out_path, visual):
         self.file_index = 0
 
         for _, __, files in os.walk(in_path):
@@ -14,7 +14,7 @@ class Vid2Frames:
                 print("[INFO] Processing: ", file)
                 path = os.path.join(in_path, file)
                 self.load_video(path)
-                self.save_frames(self.video, out_path)
+                self.save_frames(self.video, out_path, visual)
 
     # def __del__(self):
     #     del self.video
@@ -26,27 +26,31 @@ class Vid2Frames:
         if os.path.exists(path):
             self.video = cv2.VideoCapture(path)
 
-    def save_frames(self, video, out_path, visual=False):
+    def save_frames(self, video, out_path, visual):
 
         base_filename = "image_"
-
+        frame_count = 0
         while video.isOpened():
 
             ret, frame = video.read()
             if frame is not None:
-                if visual:
-                    cv2.imshow('frame', frame)
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                        break
+                if frame_count % 5 == 0:
+                    if visual:
+                        cv2.imshow('frame', frame)
+                        if cv2.waitKey(1) & 0xFF == ord('q'):
+                            break
 
-                filename = base_filename + str(self.file_index) + ".jpg"
-                cv2.imwrite(os.path.join(out_path, filename), frame)
+                    filename = base_filename + str(self.file_index) + ".jpg"
+                    cv2.imwrite(os.path.join(out_path, filename), frame)
 
-                self.file_index += 1
+                    self.file_index += 1
+                frame_count += 1
+            else:
+                break
 
         print("[INFO] Saved images", self.file_index)
         self.video.release()
 
 
 if __name__ == "__main__":
-    Vid2Frames(config.INPUT_VIDEO_PATH, config.OUTPUT_FRAMES_PATH)
+    Vid2Frames(config.INPUT_VIDEO_PATH, config.OUTPUT_FRAMES_PATH, config.VISUAL)
