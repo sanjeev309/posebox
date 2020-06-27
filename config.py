bd56a244b65e4e22e0c633185a27bdb3ed691412
Sanjeev Tripathi <sanjeevmail309@gmail.com>
2020-06-27 16:03:35 +0530

    feat(config validation): added validation of values defined in config

--- config.py
+++ config.py
@@ -6,3 +6,23 @@ This python script holds configuration for all variables to be used in this proj
 INPUT_VIDEO_PATH = "videos"
 OUTPUT_FRAMES_PATH = "frames"
 VISUAL = False
+
+
+'''
+Config validation
+'''
+import os
+import sys
+
+print("-------------------------------------")
+if not os.path.exists(INPUT_VIDEO_PATH):
+    print("Input folder not found: ", INPUT_VIDEO_PATH)
+    sys.exit(0)
+
+if not os.path.exists(OUTPUT_FRAMES_PATH):
+    print("Output folder not found: ", OUTPUT_FRAMES_PATH)
+    print("Creating folder")
+    os.makedirs(OUTPUT_FRAMES_PATH)
+
+print("Data visualisation enabled : ", VISUAL)
+print("-------------------------------------")
