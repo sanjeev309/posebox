d759e75c431a70d4fdb18dae386811f7f7ab427d
HarshiniGudipally <mailharshinig@gmail.com>
2020-06-27 22:43:22 +0530

    added(resize_frames.py)

--- resize_frames.py
+++ resize_frames.py
@@ -4,10 +4,12 @@ from PIL import Image
 
 images_dir = "frames"
 target_path = "resized_frames"
+resize_width = 512
+resize_height = 512
 for root, folders, files in os.walk(images_dir):
     for file in files:
         img = str(os.path.join(images_dir, file))
         im = Image.open(img)
-        size = (512, 512)
+        size = (resize_width, resize_height)
         im_resized = im.resize(size, Image.ANTIALIAS)
         im_resized.save(target_path + "/" + str(file))
