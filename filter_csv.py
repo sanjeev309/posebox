90c4e67487afe224c1f846e5302a1b7cc6b94eb7
Sanjeev Tripathi <sanjeevmail309@gmail.com>
2020-06-28 22:54:31 +0530

    feat(delete emmpty from csv): creating new csv file without the empty annotations

--- filter_csv.py
+++ filter_csv.py
@@ -10,11 +10,20 @@ Read CSV and delete any image from PATH that is not annotated
 with open(config.ANNOTATION_FILE) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
-    for row in csv_reader:
-        if row[3] == "0":
-            filename = row[0]
-            try:
-                os.remove(os.path.join(path, filename))
-                print("Deleted: ", filename)
-            except:
-                print("Error deleting file: ", filename)
+
+    # Open temp file to write non-empty files and annotations
+    with open('new_annotation.csv', 'w') as new_csv_file:
+        csv_writer = csv.writer(new_csv_file, delimiter=',')
+
+        for row in csv_reader:
+            # Check if empty annotation
+            if row[3] == "0":
+                filename = row[0]
+                try:
+                    # Delete that file
+                    os.remove(os.path.join(path, filename))
+                    print("Deleted: ", filename)
+                except:
+                    print("Error deleting file: ", filename)
+            else:
+                csv_writer.writerow(row)
