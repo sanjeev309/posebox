4b19708acfd319b502f2146db9deb884eb84f66a
sanjeev309 <sanjeevmail309@gmail.com>
2020-07-26 18:21:57 +0530

    fix(removed path validatation): removed unnecessary path checks, added output irectory for smoke test

--- config.py
+++ config.py
@@ -19,27 +19,7 @@ ANNOTATION_FILE = "annotation.csv"
 FORMATTED_ANNOTATION_FILE = "annotation_formatted.csv"
 HEADER_ANNOTATION_FILE = ['file_name','coordinates', 'width', 'height']
 
-'''
-Config validation
-'''
-import os
-import sys
 
-print("-------------------------------------")
-if not os.path.exists(INPUT_VIDEO_PATH):
-    print("Input folder not found: ", INPUT_VIDEO_PATH)
-    sys.exit(0)
+OUTPUT = '/home/sanjeev309/Projects/posebox/output'
 
-if not os.path.exists(OUTPUT_FRAMES_PATH):
-    print("Output folder not found: ", OUTPUT_FRAMES_PATH)
-    print("Creating folder")
-    os.makedirs(OUTPUT_FRAMES_PATH)
-
-if not os.path.exists(RESIZED_OUTPUT_FRAMES_PATH):
-    print("Resized Output folder not found: ", RESIZED_OUTPUT_FRAMES_PATH)
-    print("Creating folder")
-    os.makedirs(RESIZED_OUTPUT_FRAMES_PATH)
-
-
-print("Data visualisation enabled : ", VISUAL)
-print("-------------------------------------")
+TEST_OUTPUT = "/home/sanjeev309/Projects/posebox/smoke_test"
