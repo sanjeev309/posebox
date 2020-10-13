6770660c80918b268aca97e16245997cd8d40d1d
sanjeev309 <sanjeevmail309@gmail.com>
2020-10-13 23:25:05 +0530

    fix(model files): moved, added docstrings and config integration

--- src/model/predict_test.py
+++ src/model/predict_test.py
@@ -1,4 +1,3 @@
-
 from tensorflow import keras
 import numpy as np
 import os
@@ -6,51 +5,50 @@ import cv2
 import csv
 import config
 
-
 visualize_smoke_test = True
 
 ckpt_path = config.OUTPUT + "/ckpt" + "/weights.3400-0.0185.hdf5"
 
-data = np.empty((0,512,512,3), dtype=np.int8)
-files_store = np.empty((0,1), dtype=np.str)
-widths = np.empty((0,1), dtype=np.int)
-heights = np.empty((0,1), dtype=np.int)
+data = np.empty((0, 512, 512, 3), dtype=np.int8)
+files_store = np.empty((0, 1), dtype=np.str)
+widths = np.empty((0, 1), dtype=np.int)
+heights = np.empty((0, 1), dtype=np.int)
 
-#normalizing the images bofe sending todel
+# normalizing the images bofe sending todel
 for root, folders, files in os.walk(config.TEST_OUTPUT):
-  for file in files:
-    image_path = os.path.join(config.TEST_OUTPUT, file)
-    print(image_path)
-    image = cv2.imread(image_path)
-    width = image.shape[0]
-    height = image.shape[1]
-    print(image.shape)
-    image = image/255
-    image = np.expand_dims(image, axis=0)
-    if image is not None:
-        data = np.vstack((data, image))
-        files_store = np.append(files_store, str(file))
-        widths = np.append(widths, width)
-        heights = np.append(heights, height)
+    for file in files:
+        image_path = os.path.join(config.TEST_OUTPUT, file)
+        print(image_path)
+        image = cv2.imread(image_path)
+        width = image.shape[0]
+        height = image.shape[1]
+        print(image.shape)
+        image = image / 255
+        image = np.expand_dims(image, axis=0)
+        if image is not None:
+            data = np.vstack((data, image))
+            files_store = np.append(files_store, str(file))
+            widths = np.append(widths, width)
+            heights = np.append(heights, height)
 
 num_samples = data.shape[0]
 
-#loading the model
+# loading the model
 model = keras.models.load_model(ckpt_path)
 
-#predicting
+# predicting
 res = model.predict(data)
 res = np.multiply(res, 512)
 res = res.tolist()
 
-#storing the output from model into csv
+# storing the output from model into csv
 rows = zip(files_store, res, widths, heights)
 with open(config.OUTPUT + '/smoke_testing_results.csv', "w") as f:
     writer = csv.writer(f)
     for row in rows:
         writer.writerow(row)
 
-#drawing the coordinates on the image for visualization
+# drawing the coordinates on the image for visualization
 if visualize_smoke_test:
     if not os.path.exists(config.TEST_OUTPUT):
         os.makedirs(config.TEST_OUTPUT)
@@ -70,10 +68,13 @@ if visualize_smoke_test:
             cv2.circle(image, (int(float(res[4])), int(float(res[5]))), 2, [0, 0, 255], -1)
             cv2.circle(image, (int(float(res[6])), int(float(res[7]))), 2, [0, 0, 255], -1)
 
-            cv2.putText(image, "1", (int(float(res[0])), int(float(res[1]))), cv2.FONT_HERSHEY_SIMPLEX,1, [0,0,255],1)
-            cv2.putText(image, "2", (int(float(res[2])), int(float(res[3]))), cv2.FONT_HERSHEY_SIMPLEX,1, [0,0,255],1)
-            cv2.putText(image, "3", (int(float(res[4])), int(float(res[5]))), cv2.FONT_HERSHEY_SIMPLEX,1, [0,0,255],1)
-            cv2.putText(image, "4", (int(float(res[6])), int(float(res[7]))), cv2.FONT_HERSHEY_SIMPLEX,1, [0,0,255],1)
-
+            cv2.putText(image, "1", (int(float(res[0])), int(float(res[1]))), cv2.FONT_HERSHEY_SIMPLEX, 1, [0, 0, 255],
+                        1)
+            cv2.putText(image, "2", (int(float(res[2])), int(float(res[3]))), cv2.FONT_HERSHEY_SIMPLEX, 1, [0, 0, 255],
+                        1)
+            cv2.putText(image, "3", (int(float(res[4])), int(float(res[5]))), cv2.FONT_HERSHEY_SIMPLEX, 1, [0, 0, 255],
+                        1)
+            cv2.putText(image, "4", (int(float(res[6])), int(float(res[7]))), cv2.FONT_HERSHEY_SIMPLEX, 1, [0, 0, 255],
+                        1)
 
             cv2.imwrite(config.TEST_OUTPUT + "/" + "eval_" + str(image_name), image)
